4ee78a78cfa80bc7763b3941d69fd19e28c9328c
aishtodkar <88433899+aishtodkar@users.noreply.github.com>
2021-08-26 20:31:32 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include "header.h"
+int main()
+{
+ int a=10,b=20,c;
+// printf("Enter t");
+c=add(a,b);
+printf("Addition=%d\n",c);
+c=sub(a,b);
+printf("Addition=%d\n",c);
+return 0;
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+
+
+out: 
+	gcc main.c add.c sub.c -o out
